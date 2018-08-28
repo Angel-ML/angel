d71360ccea5d6ab95611755fd09c800a33f33044
fitzwang <fitzwang@tencent.com>
2018-08-28 16:22:59 +0800

    add long key support for embedding

--- angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/columns/UpdateColsFunc.java
+++ angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/columns/UpdateColsFunc.java
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  *
  * https://opensource.org/licenses/Apache-2.0
@@ -24,78 +24,132 @@ import com.tencent.angel.ml.math2.vector.CompIntDoubleVector;
 import com.tencent.angel.ml.math2.vector.CompIntFloatVector;
 import com.tencent.angel.ml.math2.vector.IntDoubleVector;
 import com.tencent.angel.ml.math2.vector.Vector;
+import com.tencent.angel.ml.matrix.RowType;
 import com.tencent.angel.ml.matrix.psf.update.base.PartitionUpdateParam;
 import com.tencent.angel.ml.matrix.psf.update.base.UpdateFunc;
 import com.tencent.angel.ps.storage.matrix.ServerPartition;
 import com.tencent.angel.ps.storage.vector.ServerIntDoubleRow;
 import com.tencent.angel.ps.storage.vector.ServerIntFloatRow;
+import com.tencent.angel.ps.storage.vector.ServerLongDoubleRow;
+import com.tencent.angel.ps.storage.vector.ServerLongFloatRow;
+
+import static com.tencent.angel.ml.matrix.RowType.T_DOUBLE_SPARSE;
 
 public class UpdateColsFunc extends UpdateFunc {
 
-  public UpdateColsFunc(UpdateColsParam param) {
-    super(param);
-  }
-
-  public UpdateColsFunc() {
-    super(null);
-  }
-
-  @Override public void partitionUpdate(PartitionUpdateParam partParam) {
-    PartitionUpdateColsParam param = (PartitionUpdateColsParam) partParam;
-    int[] rows = param.rows;
-    long[] cols = param.cols;
-    Vector vector = param.vector;
-
-    int matId = param.getMatrixId();
-    int partitionId = param.getPartKey().getPartitionId();
-
-    ServerPartition partition = psContext.getMatrixStorageManager().getPart(matId, partitionId);
-
-    switch (partition.getRowType()) {
-      case T_DOUBLE_DENSE:
-        ServerIntDoubleRow[] doubles = new ServerIntDoubleRow[rows.length];
-        for (int r = 0; r < rows.length; r++)
-          doubles[r] = (ServerIntDoubleRow) partition.getRow(rows[r]);
-        doUpdate((CompIntDoubleVector) vector, rows, cols, doubles);
-        return;
-      case T_FLOAT_DENSE:
-        ServerIntFloatRow[] floats = new ServerIntFloatRow[rows.length];
-        for (int r = 0; r < rows.length; r++)
-          floats[r] = (ServerIntFloatRow) partition.getRow(rows[r]);
-        doUpdate((CompIntFloatVector) vector, rows, cols, floats);
-        return;
-      default:
-        throw new AngelException("Data type should be double or float!");
+    public UpdateColsFunc(UpdateColsParam param) {
+        super(param);
     }
-  }
 
-  private void doUpdate(CompIntDoubleVector vector, int[] rows, long[] cols,
-    ServerIntDoubleRow[] doubles) {
-    double[][] updates = new double[cols.length][];
-    for (int c = 0; c < cols.length; c++)
-      updates[c] = vector.getPartitions()[c].getStorage().getValues();
-
-    for (int c = 0; c < cols.length; c++) {
-      int offset = (int) cols[c];
-      for (int r = 0; r < rows.length; r++) {
-        double v = doubles[r].get(offset);
-        doubles[r].set(offset, v + updates[c][r]);
-      }
+    public UpdateColsFunc() {
+        super(null);
     }
-  }
 
-  private void doUpdate(CompIntFloatVector vector, int[] rows, long[] cols,
-    ServerIntFloatRow[] floats) {
-    float[][] updates = new float[cols.length][];
-    for (int c = 0; c < cols.length; c++)
-      updates[c] = vector.getPartitions()[c].getStorage().getValues();
+    @Override
+    public void partitionUpdate(PartitionUpdateParam partParam) {
+        PartitionUpdateColsParam param = (PartitionUpdateColsParam) partParam;
+        int[] rows = param.rows;
+        long[] cols = param.cols;
+        Vector vector = param.vector;
 
-    for (int c = 0; c < cols.length; c++) {
-      int offset = (int) cols[c];
-      for (int r = 0; r < rows.length; r++) {
-        float v = floats[r].get(offset);
-        floats[r].set(offset, v + updates[c][r]);
-      }
+        int matId = param.getMatrixId();
+        int partitionId = param.getPartKey().getPartitionId();
+
+        ServerPartition partition = psContext.getMatrixStorageManager().getPart(matId, partitionId);
+
+        switch (partition.getRowType()) {
+            case T_DOUBLE_DENSE:
+            case T_DOUBLE_SPARSE: {
+                ServerIntDoubleRow[] doubles = new ServerIntDoubleRow[rows.length];
+                for (int r = 0; r < rows.length; r++)
+                    doubles[r] = (ServerIntDoubleRow) partition.getRow(rows[r]);
+                doUpdate((CompIntDoubleVector) vector, rows, cols, doubles);
+                return;
+            }
+            case T_DOUBLE_SPARSE_LONGKEY: {
+                ServerLongDoubleRow[] doubles = new ServerLongDoubleRow[rows.length];
+                for (int r = 0; r < rows.length; r++)
+                    doubles[r] = (ServerLongDoubleRow) partition.getRow(rows[r]);
+                doUpdate((CompIntDoubleVector) vector, rows, cols, doubles);
+                return;
+            }
+            case T_FLOAT_DENSE:
+            case T_FLOAT_SPARSE: {
+                ServerIntFloatRow[] floats = new ServerIntFloatRow[rows.length];
+                for (int r = 0; r < rows.length; r++)
+                    floats[r] = (ServerIntFloatRow) partition.getRow(rows[r]);
+                doUpdate((CompIntFloatVector) vector, rows, cols, floats);
+                return;
+            }
+            case T_FLOAT_SPARSE_LONGKEY: {
+                ServerLongFloatRow[] floats = new ServerLongFloatRow[rows.length];
+                for (int r = 0; r < rows.length; r++)
+                    floats[r] = (ServerLongFloatRow) partition.getRow(rows[r]);
+                doUpdate((CompIntFloatVector) vector, rows, cols, floats);
+                return;
+            }
+            default:
+                throw new AngelException("Data type should be double or float!");
+        }
     }
-  }
+
+    private void doUpdate(CompIntDoubleVector vector, int[] rows, long[] cols,
+                          ServerIntDoubleRow[] doubles) {
+        double[][] updates = new double[cols.length][];
+        for (int c = 0; c < cols.length; c++)
+            updates[c] = vector.getPartitions()[c].getStorage().getValues();
+
+        for (int c = 0; c < cols.length; c++) {
+            int offset = (int) cols[c];
+            for (int r = 0; r < rows.length; r++) {
+                double v = doubles[r].get(offset);
+                doubles[r].set(offset, v + updates[c][r]);
+            }
+        }
+    }
+
+    private void doUpdate(CompIntDoubleVector vector, int[] rows, long[] cols,
+                          ServerLongDoubleRow[] doubles) {
+        double[][] updates = new double[cols.length][];
+        for (int c = 0; c < cols.length; c++)
+            updates[c] = vector.getPartitions()[c].getStorage().getValues();
+
+        for (int c = 0; c < cols.length; c++) {
+            int offset = (int) cols[c];
+            for (int r = 0; r < rows.length; r++) {
+                double v = doubles[r].get(offset);
+                doubles[r].set(offset, v + updates[c][r]);
+            }
+        }
+    }
+
+    private void doUpdate(CompIntFloatVector vector, int[] rows, long[] cols,
+                          ServerIntFloatRow[] floats) {
+        float[][] updates = new float[cols.length][];
+        for (int c = 0; c < cols.length; c++)
+            updates[c] = vector.getPartitions()[c].getStorage().getValues();
+
+        for (int c = 0; c < cols.length; c++) {
+            int offset = (int) cols[c];
+            for (int r = 0; r < rows.length; r++) {
+                float v = floats[r].get(offset);
+                floats[r].set(offset, v + updates[c][r]);
+            }
+        }
+    }
+
+    private void doUpdate(CompIntFloatVector vector, int[] rows, long[] cols,
+                          ServerLongFloatRow[] floats) {
+        float[][] updates = new float[cols.length][];
+        for (int c = 0; c < cols.length; c++)
+            updates[c] = vector.getPartitions()[c].getStorage().getValues();
+
+        for (int c = 0; c < cols.length; c++) {
+            int offset = (int) cols[c];
+            for (int r = 0; r < rows.length; r++) {
+                float v = floats[r].get(offset);
+                floats[r].set(offset, v + updates[c][r]);
+            }
+        }
+    }
 }
