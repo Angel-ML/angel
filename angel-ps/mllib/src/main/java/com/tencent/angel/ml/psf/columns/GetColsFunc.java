8b84cf209764a5cbe01fc9afd901792509f18689
paynie <cswjsxs2005@gmail.com>
2018-08-30 14:12:31 +0800

    Fix long key embedding

--- angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/columns/GetColsFunc.java
+++ angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/columns/GetColsFunc.java
@@ -27,9 +27,6 @@ import com.tencent.angel.ml.matrix.psf.get.base.GetResult;
 import com.tencent.angel.ml.matrix.psf.get.base.PartitionGetParam;
 import com.tencent.angel.ml.matrix.psf.get.base.PartitionGetResult;
 import com.tencent.angel.ps.storage.matrix.ServerPartition;
-import com.tencent.angel.ps.storage.vector.ServerIntDoubleRow;
-import com.tencent.angel.ps.storage.vector.ServerIntFloatRow;
-import com.tencent.angel.ps.storage.vector.ServerLongDoubleRow;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -40,145 +37,110 @@ import java.util.List;
 import java.util.Map;
 
 public class GetColsFunc extends GetFunc {
-    Log LOG = LogFactory.getLog(GetColsFunc.class);
+  Log LOG = LogFactory.getLog(GetColsFunc.class);
 
-    public GetColsFunc(GetColsParam param) {
-        super(param);
+  public GetColsFunc(GetColsParam param) {
+    super(param);
+  }
+
+  public GetColsFunc() {
+    super(null);
+  }
+
+  @Override public PartitionGetResult partitionGet(PartitionGetParam partParam) {
+    PartitionGetColsParam param = (PartitionGetColsParam) partParam;
+    int[] rows = param.rows;
+    long[] cols = param.cols;
+    int matId = param.getMatrixId();
+    int partitionId = param.getPartKey().getPartitionId();
+    Arrays.sort(rows);
+
+    ServerPartition partition = psContext.getMatrixStorageManager().getPart(matId, partitionId);
+    Vector[] splits = new Vector[rows.length];
+    for (int i = 0; i < rows.length; i++) {
+      splits[i] = partition.getRow(rows[i]).getSplit();
     }
+    Vector result = doGet(splits, cols, partition.getRowType());
 
-    public GetColsFunc() {
-        super(null);
-    }
+    return new PartitionGetColsResult(rows, cols, result);
+  }
 
-    @Override
-    public PartitionGetResult partitionGet(PartitionGetParam partParam) {
-        PartitionGetColsParam param = (PartitionGetColsParam) partParam;
-        int[] rows = param.rows;
-        long[] cols = param.cols;
-        int matId = param.getMatrixId();
-        int partitionId = param.getPartKey().getPartitionId();
-        Arrays.sort(rows);
+  private Vector doGet(Vector[] rows, long[] cols, RowType rowType) {
+    switch (rowType) {
+      case T_DOUBLE_DENSE:
+      case T_DOUBLE_SPARSE: {
+        IntDoubleVector[] vectors = new IntDoubleVector[cols.length];
 
-        ServerPartition partition = psContext.getMatrixStorageManager().getPart(matId, partitionId);
-
-        //    System.out.print("PartitionGet ");
-        //    for (int i = 0; i < cols.length; i ++) {
-        //      System.out.print(cols[i] + " ");
-        //    }
-        //    System.out.println();
-        Vector[] splits = new Vector[rows.length];
-        for (int i = 0; i < rows.length; i++) {
-            splits[i] = partition.getRow(rows[i]).getSplit();
+        for (int i = 0; i < cols.length; i++) {
+          vectors[i] = VFactory.denseDoubleVector(rows.length);
+          for (int j = 0; j < rows.length; j++)
+            vectors[i].set(j, ((IntDoubleVector) rows[j]).get((int) cols[i]));
         }
-        Vector result = doGet(splits, cols, partition.getRowType());
+        return VFactory.compIntDoubleVector(cols.length, vectors, rows.length);
+      }
 
-        return new PartitionGetColsResult(rows, cols, result);
-    }
-
-    private Vector doGet(Vector[] rows, long[] cols, RowType rowType) {
-        //    System.out.println("doGet Double cols.length=" + cols.length);
-
-        switch (rowType) {
-            case T_DOUBLE_DENSE:
-            case T_DOUBLE_SPARSE: {
-                IntDoubleVector[] vectors = new IntDoubleVector[cols.length];
-
-                for (int i = 0; i < cols.length; i++) {
-                    vectors[i] = VFactory.denseDoubleVector(rows.length);
-                    for (int j = 0; j < rows.length; j++)
-                        vectors[i].set(j, ((IntDoubleVector) rows[j]).get((int) cols[i]));
-                }
-                return VFactory.compIntDoubleVector(cols.length, vectors, rows.length);
-            }
-            case T_DOUBLE_SPARSE_LONGKEY: {
-                IntDoubleVector[] vectors = new IntDoubleVector[cols.length];
-                for (int i = 0; i < cols.length; i++) {
-                    vectors[i] = VFactory.denseDoubleVector(rows.length);
-                    for (int j = 0; j < rows.length; j++)
-                        vectors[i].set(j, ((LongDoubleVector) rows[j]).get(cols[i]));
-                }
-                return VFactory.compIntDoubleVector(cols.length, vectors, rows.length);
-            }
-            case T_FLOAT_DENSE:
-            case T_FLOAT_SPARSE: {
-                IntFloatVector[] vectors = new IntFloatVector[cols.length];
-                for (int i = 0; i < cols.length; i++) {
-                    vectors[i] = VFactory.denseFloatVector(rows.length);
-                    for (int j = 0; j < rows.length; j++)
-                        vectors[i].set(j, ((IntFloatVector) rows[j]).get((int) cols[i]));
-                }
-                return VFactory.compIntFloatVector(cols.length, vectors, rows.length);
-            }
-            case T_FLOAT_SPARSE_LONGKEY: {
-                IntFloatVector[] vectors = new IntFloatVector[cols.length];
-                for (int i = 0; i < cols.length; i++) {
-                    vectors[i] = VFactory.denseFloatVector(rows.length);
-                    for (int j = 0; j < rows.length; j++)
-                        vectors[i].set(j, ((LongFloatVector) rows[j]).get(cols[i]));
-                }
-                return VFactory.compIntFloatVector(cols.length, vectors, rows.length);
-            }
-
-            default:
-                throw new AngelException("The rowType is not support!");
+      case T_DOUBLE_SPARSE_LONGKEY: {
+        IntDoubleVector[] vectors = new IntDoubleVector[cols.length];
+        for (int i = 0; i < cols.length; i++) {
+          vectors[i] = VFactory.denseDoubleVector(rows.length);
+          for (int j = 0; j < rows.length; j++)
+            vectors[i].set(j, ((LongDoubleVector) rows[j]).get(cols[i]));
         }
-    }
+        return VFactory.compIntDoubleVector(cols.length, vectors, rows.length);
+      }
 
-    @Override
-    public GetResult merge(List<PartitionGetResult> partResults) {
-
-        PartitionGetColsResult rr = (PartitionGetColsResult) partResults.get(0);
-        // LOG.info("Here merge");
-        if (rr.vector instanceof CompIntDoubleVector) {
-            Map<Long, Vector> maps = new HashMap<>();
-            // int sum = 0;
-            for (PartitionGetResult r : partResults) {
-                PartitionGetColsResult rrr = (PartitionGetColsResult) r;
-                long[] cols = rrr.cols;
-                //         sum += rrr.cols.length;
-                CompIntDoubleVector vector = (CompIntDoubleVector) rrr.vector;
-                for (int i = 0; i < cols.length; i++)
-                    maps.put(cols[i], vector.getPartitions()[i]);
-            }
-
-            // LOG.error("double rrr.cols = " + sum + ", map size = " + maps.size());
-
-
-            // IntDoubleVector[] allVectors = new IntDoubleVector[allColumns.length];
-            // for (int i = 0; i < allColumns.length; i ++)
-            // allVectors[i] = maps.get(allColumns[i]);
-            return new GetColsResult(maps);
-
-            // return new GetColsResult(VFactory.denseLongVector(allColumns),
-            // VFactory.compIntDoubleVector(dim, allVectors, allVectors.length));
-
-        } else if (rr.vector instanceof CompIntFloatVector) {
-            Map<Long, Vector> maps = new HashMap<>();
-            // int sum = 0;
-            for (PartitionGetResult r : partResults) {
-                PartitionGetColsResult rrr = (PartitionGetColsResult) r;
-                long[] cols = rrr.cols;
-                // sum += cols.length;
-                CompIntFloatVector vector = (CompIntFloatVector) rrr.vector;
-                for (int i = 0; i < cols.length; i++) {
-                    maps.put(cols[i], vector.getPartitions()[i]);
-                    // System.out.print(" " + cols[i]);
-                }
-                // System.out.println();
-            }
-
-            // LOG.error("float rrr.cols = " + sum + ", map size = " + maps.size());
-
-            return new GetColsResult(maps);
-            // IntFloatVector[] allVectors = new IntFloatVector[allColumns.length];
-            // for (int i = 0; i < allColumns.length; i ++)
-            // allVectors[i] = maps.get(allColumns[i]);
-
-            // return new GetColsResult(VFactory.denseLongVector(allColumns),
-            // VFactory.compIntFloatVector(dim, allVectors, allVectors.length));
-        } else {
-            throw new AngelException("Data type should be double or float!");
+      case T_FLOAT_DENSE:
+      case T_FLOAT_SPARSE: {
+        IntFloatVector[] vectors = new IntFloatVector[cols.length];
+        for (int i = 0; i < cols.length; i++) {
+          vectors[i] = VFactory.denseFloatVector(rows.length);
+          for (int j = 0; j < rows.length; j++)
+            vectors[i].set(j, ((IntFloatVector) rows[j]).get((int) cols[i]));
         }
+        return VFactory.compIntFloatVector(cols.length, vectors, rows.length);
+      }
+
+      case T_FLOAT_SPARSE_LONGKEY: {
+        IntFloatVector[] vectors = new IntFloatVector[cols.length];
+        for (int i = 0; i < cols.length; i++) {
+          vectors[i] = VFactory.denseFloatVector(rows.length);
+          for (int j = 0; j < rows.length; j++)
+            vectors[i].set(j, ((LongFloatVector) rows[j]).get(cols[i]));
+        }
+        return VFactory.compIntFloatVector(cols.length, vectors, rows.length);
+      }
+
+      default:
+        throw new AngelException("The rowType is not support!");
     }
+  }
+
+  @Override public GetResult merge(List<PartitionGetResult> partResults) {
+    PartitionGetColsResult rr = (PartitionGetColsResult) partResults.get(0);
+    if (rr.vector instanceof CompIntDoubleVector) {
+      Map<Long, Vector> maps = new HashMap<>();
+      for (PartitionGetResult r : partResults) {
+        PartitionGetColsResult rrr = (PartitionGetColsResult) r;
+        long[] cols = rrr.cols;
+        CompIntDoubleVector vector = (CompIntDoubleVector) rrr.vector;
+        for (int i = 0; i < cols.length; i++)
+          maps.put(cols[i], vector.getPartitions()[i]);
+      }
+      return new GetColsResult(maps);
+    } else if (rr.vector instanceof CompIntFloatVector) {
+      Map<Long, Vector> maps = new HashMap<>();
+      for (PartitionGetResult r : partResults) {
+        PartitionGetColsResult rrr = (PartitionGetColsResult) r;
+        long[] cols = rrr.cols;
+        CompIntFloatVector vector = (CompIntFloatVector) rrr.vector;
+        for (int i = 0; i < cols.length; i++) {
+          maps.put(cols[i], vector.getPartitions()[i]);
+        }
+      }
+      return new GetColsResult(maps);
+    } else {
+      throw new AngelException("Data type should be double or float!");
+    }
+  }
 
 }
