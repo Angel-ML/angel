7c84e44edd05eed4fbab0cbba93ebe1174756743
paynie <cswjsxs2005@gmail.com>
2018-09-06 16:31:52 +0800

    [BUGFIX] fix adam problem

--- angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/optimizer/AdamUpdateFunc.java
+++ angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/optimizer/AdamUpdateFunc.java
@@ -73,11 +73,10 @@ public class AdamUpdateFunc extends OptMMUpdateFunc {
       OptFuncs.iexpsmoothing2(square, gradient, gamma);
 
       Vector delta = OptFuncs.adamdelta(velocity, square, powBeta, powGamma);
-
+      delta.imul(stepSize);
+      weight.isub(delta);
       if (regParam != 0.0) {
-        weight.imul(1 - stepSize).iaxpy(delta, -stepSize);
-      } else {
-        weight.iaxpy(delta, -stepSize);
+        weight.imul(1 - stepSize * regParam);
       }
 
       gradient.clear();
