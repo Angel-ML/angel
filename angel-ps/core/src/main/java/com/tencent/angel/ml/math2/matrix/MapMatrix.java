5d7edce4f44d59dab1c6d2ec3a0b9daee9052912
rachelsunrh <rachelsunrh@gmail.com>
2018-12-03 15:25:19 +0800

    fix mapMatrix

--- angel-ps/core/src/main/java/com/tencent/angel/ml/math2/matrix/MapMatrix.java
+++ angel-ps/core/src/main/java/com/tencent/angel/ml/math2/matrix/MapMatrix.java
@@ -75,286 +75,286 @@ public class MapMatrix<Vec extends Vector> extends Matrix{
 
   @Override
   public Vector diag() {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector getCol(int idx) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector dot(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector transDot(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iadd(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix add(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix isub(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix sub(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix imul(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix mul(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix idiv(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix div(int rowId, Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iaxpy(int rowId, Vector other, double aplha) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix axpy(int rowId, Vector other, double aplha) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iadd(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix add(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix isub(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix sub(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix imul(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix mul(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix idiv(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix div(Vector other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iaxpy(Vector other, double aplha) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix axpy(Vector other, double aplha) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iadd(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix add(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix isub(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix sub(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix imul(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix mul(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix idiv(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix div(Matrix other) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iaxpy(Matrix other, double aplha) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix axpy(Matrix other, double aplha) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix iadd(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix add(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix isub(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix sub(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix imul(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix mul(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix idiv(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix div(double x) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public double min() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public double max() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector min(int axis) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector max(int axis) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector sum(int axis) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector average(int axis) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector std(int axis) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Vector norm(int axis) {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public Matrix copy() {
-    return null;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public int getNumRows() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public void clear() {
-
+    mapMatrix.clear();
   }
 
   @Override
   public double sum() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public double std() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public double average() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 
   @Override
   public double norm() {
-    return 0;
+    throw new UnsupportedOperationException("this operation is not support!");
   }
 }
