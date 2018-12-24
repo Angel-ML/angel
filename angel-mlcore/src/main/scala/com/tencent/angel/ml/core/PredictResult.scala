7b2e096de221ebf25cd58ff7b5b661503024fd7f
fitzwang <fitzwang@tencent.com>
2018-12-24 11:47:04 +0800

    fix bug in PredictResult

--- angel-mlcore/src/main/scala/com/tencent/angel/ml/core/PredictResult.scala
+++ angel-mlcore/src/main/scala/com/tencent/angel/ml/core/PredictResult.scala
@@ -8,7 +8,7 @@ class PredictResult(val sid: String, val pred: Double, val proba: Double, val pr
     val predStr = if (pred == Double.NaN) "" else f"$pred%.2f$separator"
     val probaStr = if (proba == Double.NaN) "" else f"$proba%.2f$separator"
     val trueLabelStr = if (trueLabel == Double.NaN) "" else f"$trueLabel%.2f"
-    f"$sid%s$separator$predLabel%.2f$predStr$probaStr$trueLabelStr"
+    f"$sid%s$separator$predLabel%.2f$separator$predStr$probaStr$trueLabelStr"
   }
 }
 
