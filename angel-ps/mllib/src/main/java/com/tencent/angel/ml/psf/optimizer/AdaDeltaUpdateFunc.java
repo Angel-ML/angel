5b8722ffe44670191980c134f99c40433098fdf9
fitzwang <fitzwang@tencent.com>
2019-01-14 10:19:34 +0800

    google format

--- angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/optimizer/AdaDeltaUpdateFunc.java
+++ angel-ps/mllib/src/main/java/com/tencent/angel/ml/psf/optimizer/AdaDeltaUpdateFunc.java
@@ -10,64 +10,69 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class AdaDeltaUpdateFunc extends OptMMUpdateFunc {
-    private static final Log LOG = LogFactory.getLog(AdaDeltaUpdateFunc.class);
 
-    public AdaDeltaUpdateFunc() {
-        super();
-    }
+  private static final Log LOG = LogFactory.getLog(AdaDeltaUpdateFunc.class);
 
-    public AdaDeltaUpdateFunc(int matId, int factor, double epsilon, double alpha, double beta, double lr, double regL1Param, double regL2Param, int epoch) {
-        super(matId, new int[]{factor}, new double[]{epsilon, alpha, beta, lr, regL1Param, regL2Param, epoch, 1});
-    }
+  public AdaDeltaUpdateFunc() {
+    super();
+  }
 
-    public AdaDeltaUpdateFunc(int matId, int factor, double epsilon, double alpha, double beta, double lr, double regL1Param, double regL2Param, int epoch, int batchSize) {
-        super(matId, new int[]{factor}, new double[]{epsilon, alpha, beta, lr, regL1Param, regL2Param, epoch, batchSize});
-    }
+  public AdaDeltaUpdateFunc(int matId, int factor, double epsilon, double alpha, double beta,
+      double lr, double regL1Param, double regL2Param, int epoch) {
+    super(matId, new int[]{factor},
+        new double[]{epsilon, alpha, beta, lr, regL1Param, regL2Param, epoch, 1});
+  }
 
-    @Override
-    public void update(ServerPartition partition, int factor, double[] scalars) {
-        double epsilon = scalars[0];
-        double alpha = scalars[1];
-        double beta = scalars[2];
-        double lr = scalars[3];
-        double l1RegParam = scalars[4];
-        double l2RegParam = scalars[5];
-        double epoch = (int) scalars[6];
-        double batchSize = (int) scalars[7];
+  public AdaDeltaUpdateFunc(int matId, int factor, double epsilon, double alpha, double beta,
+      double lr, double regL1Param, double regL2Param, int epoch, int batchSize) {
+    super(matId, new int[]{factor},
+        new double[]{epsilon, alpha, beta, lr, regL1Param, regL2Param, epoch, batchSize});
+  }
 
-        for (int f = 0; f < factor; f++) {
-            ServerRow gradientServerRow = partition.getRow(f + 3 * factor);
-            try {
-                gradientServerRow.startWrite();
-                Vector weight = partition.getRow(f).getSplit();
-                Vector square1 = partition.getRow(f + factor).getSplit();
-                Vector square2 = partition.getRow(f + 2 * factor).getSplit();
-                Vector gradient = gradientServerRow.getSplit();
+  @Override
+  public void update(ServerPartition partition, int factor, double[] scalars) {
+    double epsilon = scalars[0];
+    double alpha = scalars[1];
+    double beta = scalars[2];
+    double lr = scalars[3];
+    double l1RegParam = scalars[4];
+    double l2RegParam = scalars[5];
+    double epoch = (int) scalars[6];
+    double batchSize = (int) scalars[7];
 
-                if (batchSize > 1) {
-                    gradient.idiv(batchSize);
-                }
+    for (int f = 0; f < factor; f++) {
+      ServerRow gradientServerRow = partition.getRow(f + 3 * factor);
+      try {
+        gradientServerRow.startWrite();
+        Vector weight = partition.getRow(f).getSplit();
+        Vector square1 = partition.getRow(f + factor).getSplit();
+        Vector square2 = partition.getRow(f + 2 * factor).getSplit();
+        Vector gradient = gradientServerRow.getSplit();
 
-                OptFuncs.iexpsmoothing2(square1, gradient, alpha);
-                Vector hessian = OptFuncs.adadeltahessian(square1, square2);
-
-                if (l2RegParam != 0) {
-                    gradient.iaxpy(weight, l2RegParam);
-                }
-
-                OptFuncs.iadadeltadelta(gradient, hessian, l2RegParam);
-                weight.isub(gradient);
-                OptFuncs.iexpsmoothing2(square2, gradient, beta);
-
-                if (l1RegParam != 0) {
-                    OptFuncs.iadadeltathredshold(weight, hessian, l1RegParam, l2RegParam);
-                }
-
-                gradient.clear();
-            } finally {
-                gradientServerRow.endWrite();
-            }
-
+        if (batchSize > 1) {
+          gradient.idiv(batchSize);
         }
+
+        OptFuncs.iexpsmoothing2(square1, gradient, alpha);
+        Vector hessian = OptFuncs.adadeltahessian(square1, square2);
+
+        if (l2RegParam != 0) {
+          gradient.iaxpy(weight, l2RegParam);
+        }
+
+        OptFuncs.iadadeltadelta(gradient, hessian, l2RegParam);
+        weight.isub(gradient);
+        OptFuncs.iexpsmoothing2(square2, gradient, beta);
+
+        if (l1RegParam != 0) {
+          OptFuncs.iadadeltathredshold(weight, hessian, l1RegParam, l2RegParam);
+        }
+
+        gradient.clear();
+      } finally {
+        gradientServerRow.endWrite();
+      }
+
     }
+  }
 }
