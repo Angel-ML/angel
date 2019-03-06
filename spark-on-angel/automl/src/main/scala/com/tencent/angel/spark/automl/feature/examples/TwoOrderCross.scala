4e2152f28603707898389af4a7ddf43dc444b7f5
bluesjjw <blue.jwjiang@gmail.com>
2019-03-06 10:12:43 +0800

    update two-order cross

--- spark-on-angel/automl/src/main/scala/com/tencent/angel/spark/automl/feature/examples/TwoOrderCross.scala
+++ spark-on-angel/automl/src/main/scala/com/tencent/angel/spark/automl/feature/examples/TwoOrderCross.scala
@@ -22,22 +22,22 @@ import com.tencent.angel.spark.automl.feature.cross.FeatureCrossMeta
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
 import org.apache.spark.ml.feature.{ChiSqSelector, VectorAssembler}
-import org.apache.spark.ml.feature.operator.Cartesian
+import org.apache.spark.ml.feature.operator.{Cartesian, VectorFilterZero}
 import org.apache.spark.sql.SparkSession
 
 object TwoOrderCross {
 
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder().master("local").getOrCreate()
+    val spark = SparkSession.builder().getOrCreate()
 
-    val trainDF = spark.read.format("libsvm")
+    val data = spark.read.format("libsvm")
       .option("numFeatures", "123")
       .load("data/a9a/a9a_123d_train_trans.libsvm")
 
-    trainDF.persist()
+    data.persist()
 
-    val maxDim = FeatureUtils.maxDim(trainDF)
+    val maxDim = FeatureUtils.maxDim(data)
     println(s"max dimension: $maxDim")
 
     // feature cross meta
@@ -48,70 +48,83 @@ object TwoOrderCross {
       .setInputCol("features")
       .setOutputCol("cartesian_features_1")
 
-    val selector_1 = new ChiSqSelector()
-      .setNumTopFeatures(maxDim * maxDim / 10)
-      .setFeaturesCol("cartesian_features_1")
-      .setLabelCol("label")
-      .setOutputCol("selected_features_1")
+    val featureMap: Map[Int, Int] = Map[Int, Int]()
+
+    val filter_1 = new VectorFilterZero(featureMap)
+      .setInputCol("cartesian_features_1")
+      .setOutputCol("filter_features_1")
+
+//    val selector_1 = new ChiSqSelector()
+//      .setNumTopFeatures(maxDim * maxDim / 100)
+//      .setFeaturesCol("cartesian_features_1")
+//      .setLabelCol("label")
+//      .setOutputCol("selected_features_1")
 
     val assembler1 = new VectorAssembler()
-      .setInputCols(Array("features", "selected_features_1"))
+      .setInputCols(Array("features", "filter_features_1"))
       .setOutputCol("assemble_features_1")
 
     val cartesian_2 = new Cartesian()
       .setInputCol("assemble_features_1")
       .setOutputCol("cartesian_features_2")
 
-    val selector_2 = new ChiSqSelector()
-      .setNumTopFeatures(10 * maxDim)
-      .setFeaturesCol("cartesian_features_2")
-      .setLabelCol("label")
-      .setOutputCol("selected_features_2")
+    val filter_2 = new VectorFilterZero(featureMap)
+      .setInputCol("cartesian_features_2")
+      .setOutputCol("filter_features_2")
+
+//    val selector_2 = new ChiSqSelector()
+//      .setNumTopFeatures(10 * maxDim)
+//      .setFeaturesCol("cartesian_features_2")
+//      .setLabelCol("label")
+//      .setOutputCol("selected_features_2")
 
     val assembler_2 = new VectorAssembler()
-      .setInputCols(Array("assemble_features_1", "selected_features_2"))
+      .setInputCols(Array("assemble_features_1", "filter_features_2"))
       .setOutputCol("assemble_features_2")
 
     val pipeline = new Pipeline()
-      .setStages(Array(cartesian_1, selector_1, assembler1))
-      //.setStages(Array(cartesian_1, selector_1, assembler1, cartesian_2, selector_2, assembler_2))
+      //.setStages(Array(cartesian_1, selector_1, assembler1))
+      .setStages(Array(cartesian_1, filter_1, assembler1, cartesian_2, filter_2, assembler_2))
 
-    val featureModel = pipeline.fit(trainDF)
-    val crossDF = featureModel.transform(trainDF)
+    val featureModel = pipeline.fit(data)
+    val crossDF = featureModel.transform(data)
 
-    crossDF.persist()
-    trainDF.unpersist()
+    val splitData = crossDF.randomSplit(Array(0.7, 0.3))
+
+    val trainDF = splitData(0).persist()
+    val testDF = splitData(1).persist()
+
+    data.unpersist()
 
     println(crossDF.schema)
     crossDF.show(1)
 
-    println("non zero features in cartesian features")
-    println(FeatureUtils.countNonZero(crossDF, "cartesian_features_1").size)
+//    println("non zero features in cartesian features")
+//    println(FeatureUtils.countNonZero(crossDF, "cartesian_features_1").size)
 
     // original features
     val lr_orig = new LogisticRegression()
       .setFeaturesCol("features")
       .setMaxIter(10)
       .setRegParam(0.01)
-    val auc_orig = lr_orig.fit(crossDF).summary.asInstanceOf[BinaryLogisticRegressionSummary].areaUnderROC
+    val auc_orig = lr_orig.fit(trainDF).evaluate(testDF).asInstanceOf[BinaryLogisticRegressionSummary].areaUnderROC
     println(s"original feature: auc = $auc_orig")
 
-    // one-order cross
+    // original features + one-order cross
     val lr_cross_1 = new LogisticRegression()
-      .setFeaturesCol("cartesian_features_1")
+      .setFeaturesCol("assemble_features_1")
       .setMaxIter(10)
       .setRegParam(0.01)
-    val auc_cross_1 = lr_cross_1.fit(crossDF).summary.asInstanceOf[BinaryLogisticRegressionSummary].areaUnderROC
+    val auc_cross_1 = lr_cross_1.fit(trainDF).evaluate(testDF).asInstanceOf[BinaryLogisticRegressionSummary].areaUnderROC
     println(s"one order cross feature: auc = $auc_cross_1")
 
-    // original features + all cross features
-//    val lr_cross_2 = new LogisticRegression()
-//      .setFeaturesCol("assemble_features_2")
-//      .setLabelCol("label")
-//      .setMaxIter(10)
-//      .setRegParam(0.01)
-//    val auc_cross_2 = lr_cross_2.fit(crossDF).summary.asInstanceOf[BinaryLogisticRegressionSummary].areaUnderROC
-//    println(s"original feature + cross feature: auc = $auc_cross_2")
+    // original features + one-order cross + two-order cross features
+    val lr_cross_2 = new LogisticRegression()
+      .setFeaturesCol("assemble_features_2")
+      .setMaxIter(10)
+      .setRegParam(0.01)
+    val auc_cross_2 = lr_cross_2.fit(trainDF).evaluate(testDF).asInstanceOf[BinaryLogisticRegressionSummary].areaUnderROC
+    println(s"original feature + two-order cross feature: auc = $auc_cross_2")
 
     spark.close()
   }
