8d81f966feae2afd0723bd13105f0aa7b8e55562
paynie <cswjsxs2005@gmail.com>
2019-03-15 15:37:09 +0800

    [ISSUE-695] optimize BalanceInputFormatV2: make the split number equals to task number

--- angel-ps/core/src/main/java/com/tencent/angel/data/inputformat/BalanceInputFormatV2.java
+++ angel-ps/core/src/main/java/com/tencent/angel/data/inputformat/BalanceInputFormatV2.java
@@ -125,15 +125,19 @@ public class BalanceInputFormatV2 extends CombineTextInputFormat {
   }
 
   private long adjustMaxSize(long totalFileSize, int needSplitNum) {
-    long a = totalFileSize / needSplitNum;
-    long b = totalFileSize - a * needSplitNum;
+    if(needSplitNum <= 1) {
+      return totalFileSize;
+    }
+
+    long a = totalFileSize / (needSplitNum - 1);
+    long b = totalFileSize - a * (needSplitNum - 1);
 
     if(b == 0 || b > 0.98 * a){
       return a;
     }
 
     while(true) {
-      long delta = (a - b) / (needSplitNum + 1);
+      long delta = (a - b) / needSplitNum;
       if(delta <= 0) {
         break;
       }
