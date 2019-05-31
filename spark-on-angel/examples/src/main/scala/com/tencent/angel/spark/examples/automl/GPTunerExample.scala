4b521b3ef7f2a5d891171904a14477f62dee771c
bluesjjw <blue.jwjiang@gmail.com>
2019-05-31 06:18:25 +0200

    dependency error

--- spark-on-angel/examples/src/main/scala/com/tencent/angel/spark/examples/automl/GPTunerExample.scala
+++ spark-on-angel/examples/src/main/scala/com/tencent/angel/spark/examples/automl/GPTunerExample.scala
@@ -28,11 +28,11 @@ object GPTunerExample extends App {
 
   override def main(args: Array[String]): Unit = {
 
-    val param1: ParamSpace[Double] = new ContinuousSpace("param1", "1,10")
-    val param2: ParamSpace[Double] = new ContinuousSpace("param2", "-5,5,10")
-    val param3: ParamSpace[Double] = new DiscreteSpace[Double]("param3", "0.0,1.0,3.0,5.0")
-    val param4: ParamSpace[Int] = new DiscreteSpace[Int]("param4", "-5:5:1")
-    val solver: Solver = Solver(Array(param1, param2, param3, param4), true, surrogate ="GP")
+    val param1: ParamSpace[Double] = new ContinuousSpace("param1", "[1,10]")
+    val param2: ParamSpace[Double] = new ContinuousSpace("param2", "[-5:5:10]")
+    val param3: ParamSpace[Double] = new DiscreteSpace[Double]("param3", "{0.0,1.0,3.0,5.0}")
+    val param4: ParamSpace[Int] = new DiscreteSpace[Int]("param4", "{-5:5:1}")
+    val solver: Solver = Solver(Array(param1, param2, param3, param4), true, surrogate ="GaussianProcess")
     val trail: Trail = new TestTrail()
     (0 until 100).foreach{ iter =>
       println(s"------iteration $iter starts------")
