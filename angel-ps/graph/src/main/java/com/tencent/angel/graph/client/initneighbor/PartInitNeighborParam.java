183875626d63f1512a4903e58c44e9ff177a2813
yaochi <yaochi@sugo.io>
2019-06-05 11:22:54 +0800

    fix the indent

--- angel-ps/graph/src/main/java/com/tencent/angel/graph/client/initneighbor/PartInitNeighborParam.java
+++ angel-ps/graph/src/main/java/com/tencent/angel/graph/client/initneighbor/PartInitNeighborParam.java
@@ -24,92 +24,90 @@ import com.tencent.angel.graph.data.NodeEdgesPair;
 import com.tencent.angel.ml.matrix.psf.update.base.PartitionUpdateParam;
 import io.netty.buffer.ByteBuf;
 
-import java.util.Map;
-
 public class PartInitNeighborParam extends PartitionUpdateParam {
-    private NodeEdgesPair[] nodeEdgesPairs;
+	private NodeEdgesPair[] nodeEdgesPairs;
 
-    /**
-     * Store position: start index in nodeIds
-     */
-    private int startIndex;
+	/**
+	 * Store position: start index in nodeIds
+	 */
+	private int startIndex;
 
-    /**
-     * Store position: end index in nodeIds
-     */
-    private int endIndex;
+	/**
+	 * Store position: end index in nodeIds
+	 */
+	private int endIndex;
 
-    public PartInitNeighborParam(int matrixId, PartitionKey partKey, NodeEdgesPair[] nodeEdgesPairs
-            , int startIndex, int endIndex) {
-        super(matrixId, partKey);
-        this.nodeEdgesPairs = nodeEdgesPairs;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-    }
+	public PartInitNeighborParam(int matrixId, PartitionKey partKey, NodeEdgesPair[] nodeEdgesPairs
+					, int startIndex, int endIndex) {
+		super(matrixId, partKey);
+		this.nodeEdgesPairs = nodeEdgesPairs;
+		this.startIndex = startIndex;
+		this.endIndex = endIndex;
+	}
 
-    public PartInitNeighborParam() {
-        this(0, null, null, 0, 0);
-    }
+	public PartInitNeighborParam() {
+		this(0, null, null, 0, 0);
+	}
 
-    public NodeEdgesPair[] getNodeEdgesPairs() {
-        return nodeEdgesPairs;
-    }
+	public NodeEdgesPair[] getNodeEdgesPairs() {
+		return nodeEdgesPairs;
+	}
 
-    @Override
-    public void serialize(ByteBuf buf) {
-        super.serialize(buf);
+	@Override
+	public void serialize(ByteBuf buf) {
+		super.serialize(buf);
 
-        buf.writeInt(endIndex - startIndex);
+		buf.writeInt(endIndex - startIndex);
 
-        for (int i = startIndex; i < endIndex; i++) {
-            NodeEdgesPair nodeEdgesPair = nodeEdgesPairs[i];
-            nodeEdgesPair.getNode().serialize(buf);
+		for (int i = startIndex; i < endIndex; i++) {
+			NodeEdgesPair nodeEdgesPair = nodeEdgesPairs[i];
+			nodeEdgesPair.getNode().serialize(buf);
 
-            Edge[] edges = nodeEdgesPair.getEdges();
-            int numEdges = edges.length;
-            buf.writeInt(numEdges);
+			Edge[] edges = nodeEdgesPair.getEdges();
+			int numEdges = edges.length;
+			buf.writeInt(numEdges);
 
-            for (Edge edge : edges) {
-                edge.serialize(buf);
-            }
-        }
-    }
+			for (Edge edge : edges) {
+				edge.serialize(buf);
+			}
+		}
+	}
 
-    @Override
-    public void deserialize(ByteBuf buf) {
-        super.deserialize(buf);
+	@Override
+	public void deserialize(ByteBuf buf) {
+		super.deserialize(buf);
 
-        int nodeEdgesNum = buf.readInt();
+		int nodeEdgesNum = buf.readInt();
 
-        nodeEdgesPairs = new NodeEdgesPair[nodeEdgesNum];
-        for (int i = 0; i < nodeEdgesNum; i++) {
-            Node node = new Node();
-            node.deserialize(buf);
+		nodeEdgesPairs = new NodeEdgesPair[nodeEdgesNum];
+		for (int i = 0; i < nodeEdgesNum; i++) {
+			Node node = new Node();
+			node.deserialize(buf);
 
-            int numEdges = buf.readInt();
-            Edge[] edges = new Edge[numEdges];
+			int numEdges = buf.readInt();
+			Edge[] edges = new Edge[numEdges];
 
-            for (int j = 0; j < numEdges; j++) {
-                Edge edge = new Edge();
-                edge.deserialize(buf);
-                edges[j] = edge;
-            }
-            nodeEdgesPairs[i] = new NodeEdgesPair(node, edges);
-        }
-    }
+			for (int j = 0; j < numEdges; j++) {
+				Edge edge = new Edge();
+				edge.deserialize(buf);
+				edges[j] = edge;
+			}
+			nodeEdgesPairs[i] = new NodeEdgesPair(node, edges);
+		}
+	}
 
-    @Override
-    public int bufferLen() {
-        int len = super.bufferLen();
-        len += 4;
-        for (NodeEdgesPair nodeEdgesPair : nodeEdgesPairs) {
-            len += nodeEdgesPair.getNode().bufferLen();
+	@Override
+	public int bufferLen() {
+		int len = super.bufferLen();
+		len += 4;
+		for (NodeEdgesPair nodeEdgesPair : nodeEdgesPairs) {
+			len += nodeEdgesPair.getNode().bufferLen();
 
-            len += 4;
-            for (Edge edge : nodeEdgesPair.getEdges()) {
-                len += edge.bufferLen();
-            }
-        }
-        return len;
-    }
+			len += 4;
+			for (Edge edge : nodeEdgesPair.getEdges()) {
+				len += edge.bufferLen();
+			}
+		}
+		return len;
+	}
 }
