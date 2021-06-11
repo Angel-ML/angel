4f52e289c4154eff81fcb498119811eaf5ba322d
howiehywang <howiehywang@tencent.com>
2021-06-11 19:22:44 +0800

    import HANP

--- spark-on-angel/examples/src/main/scala/com/tencent/angel/spark/examples/local/HanpExample.scala
+++ spark-on-angel/examples/src/main/scala/com/tencent/angel/spark/examples/local/HanpExample.scala
@@ -3,6 +3,7 @@ package com.tencent.angel.spark.examples.local
 import com.tencent.angel.graph.utils.GraphIO
 import com.tencent.angel.spark.context.PSContext
 import com.tencent.angel.spark.ml.core.ArgsUtil
+import com.tencent.angel.graph.community.hanp.HANP
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
