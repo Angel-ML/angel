d5b90585807c44bfb4c75b9423d33f12f57b6f9e
rachelsunrh <rachelsunrh@gmail.com>
2025-06-10 17:09:52 +0800

    Feat: update submit script
    Signed-off-by: rachelsunrh <rachelsunrh@gmail.com>

--- spark-on-angel/bin/spark-on-angel-env.sh
+++ spark-on-angel/bin/spark-on-angel-env.sh
@@ -14,11 +14,11 @@ export ANGEL_VERSION=<ANGEL_VERSION>
 
 
 scala_jar=scala-library-2.11.8.jar
-angel_ps_external_jar=fastutil-7.1.0.jar,htrace-core-2.05.jar,sizeof-0.3.0.jar,kryo-shaded-4.0.0.jar,minlog-1.3.0.jar,memory-0.8.1.jar,commons-pool-1.6.jar,netty-all-4.1.42.Final.jar,hll-1.6.0.jar,stream-2.7.0.jar
+angel_ps_external_jar=chill_2.11-0.9.3.jar,chill-java-0.9.3.jar,fastutil-7.1.0.jar,htrace-core-2.05.jar,sizeof-0.3.0.jar,kryo-shaded-4.0.0.jar,minlog-1.3.0.jar,memory-0.8.1.jar,commons-pool-1.6.jar,netty-all-4.1.42.Final.jar,hll-1.6.0.jar,stream-2.7.0.jar
 angel_ps_jar=angel-ps-graph-${ANGEL_VERSION}.jar,angel-ps-core-${ANGEL_VERSION}.jar,angel-ps-psf-${ANGEL_VERSION}.jar,angel-ps-mllib-${ANGEL_VERSION}.jar,spark-on-angel-mllib-${ANGEL_VERSION}-ps.jar,spark-on-angel-graph-${ANGEL_VERSION}-ps.jar
 
 sona_jar=spark-on-angel-core-${ANGEL_VERSION}.jar,spark-on-angel-mllib-${ANGEL_VERSION}.jar,spark-on-angel-graph-${ANGEL_VERSION}.jar
-sona_external_jar=fastutil-7.1.0.jar,htrace-core-2.05.jar,sizeof-0.3.0.jar,kryo-shaded-4.0.0.jar,minlog-1.3.0.jar,memory-0.8.1.jar,commons-pool-1.6.jar,netty-all-4.1.42.Final.jar,hll-1.6.0.jar,json4s-jackson_2.11-3.5.3.jar,stream-2.7.0.jar,jniloader-1.1.jar,native_system-java-1.1.jar,arpack_combined_all-0.1.jar,core-1.1.2.jar,netlib-native_ref-linux-armhf-1.1-natives.jar,netlib-native_ref-linux-i686-1.1-natives.jar,netlib-native_ref-linux-x86_64-1.1-natives.jar,netlib-native_system-linux-armhf-1.1-natives.jar,netlib-native_system-linux-i686-1.1-natives.jar,netlib-native_system-linux-x86_64-1.1-natives.jar,algs4-1.0.3.jar
+sona_external_jar=chill_2.11-0.9.3.jar,chill-java-0.9.3.jar,fastutil-7.1.0.jar,htrace-core-2.05.jar,sizeof-0.3.0.jar,kryo-shaded-4.0.0.jar,minlog-1.3.0.jar,memory-0.8.1.jar,commons-pool-1.6.jar,netty-all-4.1.42.Final.jar,hll-1.6.0.jar,json4s-jackson_2.11-3.5.3.jar,stream-2.7.0.jar,jniloader-1.1.jar,native_system-java-1.1.jar,arpack_combined_all-0.1.jar,core-1.1.2.jar,netlib-native_ref-linux-armhf-1.1-natives.jar,netlib-native_ref-linux-i686-1.1-natives.jar,netlib-native_ref-linux-x86_64-1.1-natives.jar,netlib-native_system-linux-armhf-1.1-natives.jar,netlib-native_system-linux-i686-1.1-natives.jar,netlib-native_system-linux-x86_64-1.1-natives.jar,algs4-1.0.3.jar
 
 dist_jar=${angel_ps_external_jar},${angel_ps_jar},${scala_jar},${sona_jar}
 local_jar=${sona_external_jar},${angel_ps_jar},${sona_jar}
